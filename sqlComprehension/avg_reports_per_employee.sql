98056e012e43f44763495f660e7c9634b3c758f3
adroit <adroit.vukosi@gmail.com>
2023-02-20 22:16:58 +0200

    average number of reports per employee & employees with reports higher than average

--- sqlComprehension/avg_reports_per_employee.sql
+++ sqlComprehension/avg_reports_per_employee.sql
@@ -1,3 +1,30 @@
-#Note: Not yet done, Still to find out what they mean by report. Is it employees that report to other empoyee or is it all orders that the employee captured or what? Question not clear enough.
-SELECT employee_id, last_name, first_name,reports_to
-FROM public.employees;
+with orders_per_employee as (
+	SELECT employees.employee_id, count(orders.order_id) as order_count
+	FROM employees
+	inner join orders
+	on employees.employee_id = orders.employee_id
+	group by employees.employee_id
+)
+select avg(order_count) repors_avarage
+from orders_per_employee;
+
+with orders_per_employeetwo as (
+	SELECT employees.employee_id, count(orders.order_id) as order_count
+	FROM employees
+	inner join orders
+	on employees.employee_id = orders.employee_id
+	group by employees.employee_id
+)
+select count(employee_id) employees_with_reports_more_than_avarage
+from orders_per_employeetwo
+where order_count > (
+	with orders_per_employee as (
+		SELECT employees.employee_id, count(orders.order_id) as order_count
+		FROM employees
+		inner join orders
+		on employees.employee_id = orders.employee_id
+		group by employees.employee_id
+	)
+	select avg(order_count) order_avarage
+	from orders_per_employee
+);
