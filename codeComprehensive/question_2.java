07fef6e5eb3ad760445290395cbdb4e99383bcce
adroit <adroit.vukosi@gmail.com>
2023-02-20 11:49:22 +0200

    Code Comprehensive Classes

---
+++ codeComprehensive/question_2.java
@@ -0,0 +1,38 @@
+package codeComprehensive;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class question_2 {
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) throws Exception {
+        // Sort the integers
+        Arrays.sort(arr);
+        long minSum, maxSum;
+        if (arr.length == 5) {
+            minSum = arr[0] + arr[1] + arr[2] + arr[3];
+            maxSum = arr[4] + arr[3] + arr[2] + arr[1];
+            System.out.print(minSum + " " + maxSum);
+        } else {
+            throw new Exception("Integer Array Size must equal to 5");
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
---
+++ codeComprehensive/question_4.java
@@ -0,0 +1,49 @@
+package codeComprehensive;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class question_4 {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(long[] arr) throws Exception {
+        // Sort the integers
+        Arrays.sort(arr);
+        long minSum = 0, maxSum = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] >= 1 && arr[i] <= 1000) {
+                minSum+= arr[i];
+            } else {
+                throw new Exception("Integer Value Out Of Constraints");
+            }
+        }
+
+        for (int i = 4; i > 0; i--) {
+            if (arr[i] >= 1 && arr[i] <= 1000) {
+                maxSum+= arr[i];
+            } else {
+                throw new Exception("Integer Value Out Of Constraints");
+            }
+        }
+        System.out.print(minSum + " " + maxSum);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        long[] arr = new long[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            long arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
