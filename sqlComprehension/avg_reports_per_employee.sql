6f2e0175128860753937574059e2754f1d6887bd
adroit <adroit.vukosi@gmail.com>
2023-02-20 13:10:39 +0200

    Not yet done, still need to get clarity on what a report is on this regard.

--- sqlComprehension/avg_reports_per_employee.sql
+++ sqlComprehension/avg_reports_per_employee.sql
@@ -1,4 +1,3 @@
+#Note: Not yet done, Still to find out what they mean by report. Is it employees that report to other empoyee or is it all orders that the employee captured or what? Question not clear enough.
 SELECT employee_id, last_name, first_name,reports_to
 FROM public.employees;
-
-#Still to find out what they mean by report
